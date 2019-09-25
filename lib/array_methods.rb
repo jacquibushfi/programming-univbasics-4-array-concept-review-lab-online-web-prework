c40bc74cf82f58959d464cf0ac13f89e98cb7859
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-25 18:19:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -24,11 +24,11 @@
 
  def find_max_value(array)
    x = array[0]
-   array.length.times do { |index|
+   array.length.times do  |index|
      if array[index] > x
        x = array[index]
      end
-    }
+   end
      x
   end
 
