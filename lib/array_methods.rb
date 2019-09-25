37965dc34ee935e13a405469484b6f459bd6059a
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-25 18:14:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,16 +22,16 @@
 # end
 
 
-# def find_max_value(array)
-#   x = array[0]
-#   array.length.times do { |index|}
- #    if array.[index] > x
- #      x = array[index]
- #    end
- #    }
-#     x
-#  end
-#  end
+ def find_max_value(array)
+   x = array[0]
+   array.length.times do { |index|}
+     if array[index] > x
+       x = array[index]
+     end
+    }
+     x
+  end
+
 
 def find_min_value(array)
   # Add your solution here
